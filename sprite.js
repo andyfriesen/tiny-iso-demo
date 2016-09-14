1e70b83d49d7bb6d16145874a3e1841a2ce511e4
Andy Friesen <andy.friesen@gmail.com>
2016-09-13 17:18:07 -0700

    class Sprite.

--- sprite.js
+++ sprite.js
@@ -31,23 +31,53 @@ function right(x, y) {
     return p;
 }
 
-function Sprite(div, x, y, width, height) {
-    this.div = div;
-    this.x = x || 0;
-    this.y = y || 0;
-    this.width = width || 0;
-    this.height = height || 0;
-}
+class Sprite {
+    constructor(div, x, y, width, height) {
+        this.div = div;
+        this.x = x || 0;
+        this.y = y || 0;
+        this.width = width || 0;
+        this.height = height || 0;
+    }
 
-Sprite.prototype.DOWN = 'DOWN';
-Sprite.prototype.UP = 'UP';
-Sprite.prototype.LEFT = 'LEFT';
-Sprite.prototype.RIGHT = 'RIGHT';
+    move(x, y) {
+        this.x = x;
+        this.y = y;
+        this.update();
+    }
 
-Sprite.prototype.move = function(x, y) {
-    this.x = x;
-    this.y = y;
-    this.update();
+    getScreenPos() {
+        return [this.x - this.y,
+                this.y / 2 + this.x / 2];
+    }
+
+    getTilePos() {
+        var x = Math.round(this.x / 32);
+        var y = Math.round(this.y / 32);
+
+        var tp = getTilePos(x, y);
+
+        var stx = map.tileToScreen(tp[0], tp[1]);
+
+        if (oldX != stx[0] || oldY != stx[1]) {
+            oldX = stx[0];
+            oldY = stx[1];
+        }
+
+        marker.style.left = stx[0] + 32;
+        marker.style.top = stx[1] + 16;
+    }
+
+    update() {
+        this.getTilePos();
+
+        var p = this.getScreenPos();
+        this.div.style.left = p[0] + this.width / 2;
+        this.div.style.top = p[1] + this.height / 2;
+        this.div.style.width = this.width;
+        this.div.style.height = this.height;
+        this.div.style.zIndex = 9999;//Math.floor(y / 16);
+    }
 }
 
 var marker = document.createElement('div');
@@ -63,46 +93,6 @@ document.body.appendChild(marker);
 var oldX = 0;
 var oldY = 0;
 
-Sprite.prototype.getScreenPos = function() {
-    return [this.x - this.y,
-            this.y / 2 + this.x / 2];
-}
-
 function getTilePos(x, y) {
     return [x, y];
-}
-
-function ASSERT(f, a, b, result) {
-    var actual = f(a, b);
-    if (actual.toSource() != result.toSource()) {
-        console.log("FAIL: " + {f:f.name, a:a, b:b, expected:result, actual:actual}.toSource());
-    }
-}
-
-Sprite.prototype.getTilePos = function() {
-    var x = Math.round(this.x / 32);
-    var y = Math.round(this.y / 32);
-
-    var tp = getTilePos(x, y);
-
-    var stx = map.tileToScreen(tp[0], tp[1]);
-
-    if (oldX != stx[0] || oldY != stx[1]) {
-        oldX = stx[0];
-        oldY = stx[1];
-    }
-
-    marker.style.left = stx[0] + 32;
-    marker.style.top = stx[1] + 16;
-}
-
-Sprite.prototype.update = function() {
-    this.getTilePos();
-
-    var p = this.getScreenPos();
-    this.div.style.left = p[0] + this.width / 2;
-    this.div.style.top = p[1] + this.height / 2;
-    this.div.style.width = this.width;
-    this.div.style.height = this.height;
-    this.div.style.zIndex = 9999;//Math.floor(y / 16);
 }
