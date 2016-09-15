a686cc31a621cc9e2f65b2599476602017d36ddc
Andy Friesen <andy.friesen@gmail.com>
2016-09-14 18:38:41 -0700

    Use class in tilemovement.js

--- tilemovement.js
+++ tilemovement.js
@@ -1,96 +1,91 @@
-function Actor(parent) {
-    this.x = 0;
-    this.y = 0;
-    this.width = 32;
-    this.height = 32;
 
-    this.div = document.createElement('div');
-    this.div.className = 'actor';
+class Actor {
+    constructor(parent) {
+        this.x = 0;
+        this.y = 0;
+        this.width = 32;
+        this.height = 32;
 
-    this.sprite = new Sprite(this.div, this.x, this.y, this.width, this.height);
+        this.div = document.createElement('div');
+        this.div.className = 'actor';
 
-    var parentDiv = (typeof parent == 'undefined'
-                     ? document.body
-                     : parent.sprite.div);
-    parentDiv.appendChild(this.div);
+        this.sprite = new Sprite(this.div, this.x, this.y, this.width, this.height);
+
+        var parentDiv = (typeof parent == 'undefined'
+                        ? document.body
+                        : parent.sprite.div);
+        parentDiv.appendChild(this.div);
+    }
+
+    tick() {
+    };
+
+    update() {
+        this.sprite.move(this.x, this.y);
+    };
+
+    traverse(fn) {
+        return fn(this);
+    }
+
+    touches(x, y, w, h) {
+        var right = this.x + this.width;
+        var bottom = this.y + this.height;
+
+        var oright = x + w;
+        var obottom = y + h;
+
+        var result;
+        if ((this.x >= oright ||
+            this.y >= obottom ||
+            x >= right ||
+            y >= bottom)
+        ) {
+            return false;
+        } else {
+            return true;
+        }
+    }
 }
-
-Actor.prototype = {};
 
 Actor.prototype.typename = 'Actor';
 
-Actor.prototype.tick = function() {
-};
-
-Actor.prototype.update = function() {
-    this.sprite.move(this.x, this.y);
-};
-
-Actor.prototype.traverse = function(fn) {
-    return fn(this);
-}
-
-Actor.prototype.touches = function(x, y, w, h) {
-    var right = this.x + this.width;
-    var bottom = this.y + this.height;
-
-    var oright = x + w;
-    var obottom = y + h;
-
-    var result;
-    if ((this.x >= oright ||
-         this.y >= obottom ||
-         x >= right ||
-         y >= bottom)
-    ) {
-        return false;
-    } else {
-        return true;
-    }
-};
-
 ///
 
-function ActorCollection() {
-    Actor.call(this);
+class ActorCollection extends Actor {
+    constructor() {
+        super();
+        this.div.className += ' actorcollection';
+        this.children = [];
+    }
 
-    this.div.className += ' actorcollection';
+    traverse(fn) {
+        for (var i = 0; i < this.children.length; i++) {
+            var result = this.children[i].traverse(fn);
+            if (!result) {
+                return result;
+            }
+        }
+    }
 
-    this.children = [];
+    touches(x, y, w, h) {
+        return false;
+    }
+
+    tick() {
+        for (var i = 0; i < this.children.length; i++) {
+            this.children[i].tick();
+        }
+    }
 }
-
-subclass(Actor, ActorCollection);
 
 ActorCollection.prototype.typename = 'ActorCollection';
 
-ActorCollection.prototype.traverse = function(fn) {
-    for (var i = 0; i < this.children.length; i++) {
-        var result = this.children[i].traverse(fn);
-        if (!result) {
-            return result;
-        }
-    }
-};
-
-ActorCollection.prototype.touches = function(x, y, w, h) {
-    return false;
-};
-
-ActorCollection.prototype.tick = function() {
-    for (var i = 0; i < this.children.length; i++) {
-        this.children[i].tick();
-    }
-};
-
 ///
 
-function RootActor() {
-    ActorCollection.call(this);
-}
-
-subclass(ActorCollection, RootActor);
-
-RootActor.prototype.update = function() {
+class RootActor extends ActorCollection {
+    update() {
+    }
 }
 
 ///
@@ -113,67 +108,67 @@ Obstruction.prototype.typename = 'Obstruction';
 
 ///
 
-function Player(input, root) {
-    Actor.call(this, root);
+class Player extends Actor {
+    constructor(input, root) {
+        super(root);
+        this.div.className += ' player';
 
-    this.div.className += ' player';
-
-    this.input = input;
-    this.root = root;
-    this.update();
-}
-
-subclass(Actor, Player);
-Player.prototype.typename = 'Player';
-
-Player.prototype.tick = function() {
-    SPEED = 32;
-    var px = this.x;
-    var py = this.y;
-    if (this.input.up()) {
-        py -= SPEED;
-    } else if (this.input.down()) {
-        py += SPEED;
-    } else if (this.input.left()) {
-        px -= SPEED;
-    } else if (this.input.right()) {
-        px += SPEED;
+        this.input = input;
+        this.root = root;
+        this.update();
     }
 
-    var result = [];
-    function touches(node) {
-        if (node != this && node.touches(px, py, this.width, this.height)) {
-            result.push(node);
+    tick() {
+        let SPEED = 32;
+        var px = this.x;
+        var py = this.y;
+        if (this.input.up()) {
+            py -= SPEED;
+        } else if (this.input.down()) {
+            py += SPEED;
+        } else if (this.input.left()) {
+            px -= SPEED;
+        } else if (this.input.right()) {
+            px += SPEED;
         }
-        return true;
-    }
 
-    if (px == this.x && py == this.y) {
-        return;
-    }
+        var result = [];
+        function touches(node) {
+            if (node != this && node.touches(px, py, this.width, this.height)) {
+                result.push(node);
+            }
+            return true;
+        }
 
-    var tileX = Math.floor(px / SPEED);
-    var tileY = Math.floor(py / SPEED);
-    console.log("Tile pos: ", Math.floor(this.x / SPEED),
-                                Math.floor(this.y / SPEED),
-                                tileX,
-                                tileY);
-    if ((tileX < 0 ||
-         tileY < 0 ||
-         tileY >= TILES.length ||
-         tileX >= TILES[tileY].length)
-    ) {
-        return;
-    }
+        if (px == this.x && py == this.y) {
+            return;
+        }
 
-    if ((3 == (TILES[tileY][tileX] & 3))) {
-        return;
-    }
+        var tileX = Math.floor(px / SPEED);
+        var tileY = Math.floor(py / SPEED);
+        console.log("Tile pos: ", Math.floor(this.x / SPEED),
+                                    Math.floor(this.y / SPEED),
+                                    tileX,
+                                    tileY);
+        if ((tileX < 0 ||
+            tileY < 0 ||
+            tileY >= TILES.length ||
+            tileX >= TILES[tileY].length)
+        ) {
+            return;
+        }
 
-    this.x = px;
-    this.y = py;
-    this.update();
+        if ((3 == (TILES[tileY][tileX] & 3))) {
+            return;
+        }
+
+        this.x = px;
+        this.y = py;
+        this.update();
+    }
 }
+
+Player.prototype.typename = 'Player';
 
 ///
 
@@ -188,6 +183,7 @@ Engine.prototype.tick = function() {
 };
 
 ///
+
 
 var map = new Map(document.getElementById('map'), TILES);
 
